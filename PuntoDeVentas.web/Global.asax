e8b14c7fd4908580080b6c6c3b2758bd46e7029b
julio <julioolivares90@gmail.com>
2019-11-21 20:01:03 -0600

    proyecto webforms
    proyecto webforms

---
+++ PuntoDeVentas.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PuntoDeVentas.web.Global" Language="C#" %>
---
+++ PuntoDeVentas/VistaReportes/ReporteDeVentas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntoDeVentas.VistaReportes
+{
+    public partial class ReporteDeVentas : Form
+    {
+        public ReporteDeVentas()
+        {
+            InitializeComponent();
+        }
+
+        private void ReporteDeVentas_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
